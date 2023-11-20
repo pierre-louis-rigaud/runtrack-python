c7d13ff74bb86d13cc327c682f9bb2c6f5efc159
CoinFLip2 <pierrelouis3131@gmail.com>
2023-11-20 11:21:14 +0100

    Job08

---
+++ Jour01/Job08/Job08.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+def add(num1, num2):
+    return num1 * num2
+
+
+print(num1, "x", num2, "=", add(num1, num2))
