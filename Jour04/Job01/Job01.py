c16d4062d70a4914d043c22d190bbddf9dcf8d62
CoinFLip2 <pierrelouis3131@gmail.com>
2023-11-23 14:16:53 +0100

    Job 1 et 2

---
+++ Jour04/Job01/Job01.py
@@ -0,0 +1,6 @@
+def liste():
+    fruits = ['pomme', 'cerise', 'orange']
+    return fruits
+print(liste())
+
+    
---
+++ Jour04/Job02/Job02.py
@@ -0,0 +1,7 @@
+def liste():
+    fruits = "pomme cerise orange"
+    deux_element = fruits[5:12:1]
+    return deux_element
+print(liste())
+
+    
